bb1a1e507b4d5f2dad718e6514d66f4792242805
Marco Kurzynski <marcokurzynski@icloud.com>
2023-09-27 22:54:03 +0000

    seems run run hello world script

--- go.sh
+++ go.sh
@@ -1,6 +1,38 @@
 #!/bin/bash -e
 
 CONTAINER=gem5cxl
+BASE="$(cd -P "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-docker build -t $CONTAINER .
-docker run --rm -itv .:/mnt -w /mnt/gem5 $CONTAINER bash -c "printf '\n' | scons ./build/X86/gem5.opt -j`nproc`"
+build_container() {
+  docker build -t $CONTAINER $BASE
+}
+
+build_gem5() {
+  docker run --rm -itv $BASE:/mnt -w /mnt/gem5 $CONTAINER bash -c "printf '\n' | scons ./build/X86/gem5.opt -j`nproc`"
+}
+
+build_m5() {
+  docker run --rm -itv $BASE:/mnt -w /mnt/gem5/util/m5 $CONTAINER scons ./build/x86/out/m5 -j`nproc`
+}
+
+build_disk() {
+  docker run --privileged --rm -itv $BASE:/mnt -w /mnt/disk-image $CONTAINER bash build.sh
+}
+
+run() {
+  docker run --privileged --rm -itv $BASE:/mnt -w /mnt $CONTAINER gem5/build/X86/gem5.opt x86-ubuntu-run-cxl-kvm.py
+}
+
+main() {
+  build_container
+  build_gem5
+  build_m5
+  build_disk
+  run
+}
+
+if [[ -z "$1" ]]; then
+    main
+else
+    "$@"
+fi
